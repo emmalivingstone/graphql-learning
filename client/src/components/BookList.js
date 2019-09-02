bb41c0a65c1e48bba5d1d47134a210c0a9d5eb7c
Emma Livingstone <emmalivingstone94@gmail.com>
2019-09-02 15:48:53 +0100

    Deleting uneeded files from client, adding book list component

---
+++ client/src/components/BookList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class BookList extends React.Component {
+  render() {
+    return (
+      <div id='main'>
+        <ul id='book-list'>
+          <li>Book Name</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BookList;
